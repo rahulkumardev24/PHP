6dbf62ac9ae35c99dd601bca7d270ec4274d5c15
rahul <rkrahulroy151617@gmail.com>
2025-09-25 16:39:47 +0530

    Learn Cookies

--- cookies/cookies.php
+++ cookies/cookies.php
@@ -1,4 +1,14 @@
 <?php
 
+// Set cookie (expires in 1 day)
+setcookie("fruit", "apple", time() + 86400);
 
-?>
+if (isset($_COOKIE['fruit'])) {
+    echo "Current Cookie is: " . $_COOKIE['fruit'] . "<br>";
+
+    // Show all cookies
+    echo "All Cookies: ";
+    print_r($_COOKIE);
+} else {
+    echo "No Cookie";
+}
