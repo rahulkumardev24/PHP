c69f74d68f2dbfbfc1bb868842b5122f64bf75bf
rahul <rkrahulroy151617@gmail.com>
2025-09-30 12:51:32 +0530

    simple website with PHP

---
+++ simple website/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+      <p>&copy; 2025 Your Website. All Rights Reserved.</p>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="contact.php">Contact</a></li>
+  </ul>
+
+</footer>
---
+++ simple website/header.php
@@ -0,0 +1,11 @@
+<div class="header">
+
+    <h3 class="logo">LOGO</h3>
+    <ul>
+        <li class=""> <a href="index.php">Home</a></li>
+        <li class=""> <a href="about.php">About</a></li>
+        <li class=""><a href="contact.php">Contact</a></li>
+    
+    </ul>
+
+</div>
