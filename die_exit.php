90763d0caef3d46024c1b412f24a6aae3d901920
rahul <rkrahulroy151617@gmail.com>
2025-09-30 13:09:15 +0530

    die and exit

---
+++ die_exit.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+echo $a - $b;
+echo "<br/>";
+
+// die; 
+// die("Here die") ; 
+
+// Exit 
+// exit ; 
+exit("Here Exit") ;
+
+echo $a + $b;
+echo "<br/>";
+
+echo $a * $b;
+echo "<br/>";
+
+echo $a / $b;
+echo "<br/>" ; 
